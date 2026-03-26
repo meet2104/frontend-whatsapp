aeda050bfd2ca3ccda993fb196d6fd3a3d12e045
Meet <goswamimeet380@gmail.com>
2026-03-26 16:23:50 +0530

    changes

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : "/api",
 });
 
 API.interceptors.request.use((config) => {
